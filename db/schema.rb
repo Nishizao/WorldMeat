9c63ed493edb599e63b59025df1aaa9c57956ac4
Nishizao <nishizao93@icloud.com>
2018-10-08 05:34:49 +0000

    create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_080815) do
+ActiveRecord::Schema.define(version: 2018_10_08_052459) do
 
   create_table "recettes", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.string "etapes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
